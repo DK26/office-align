5b052c3902a2a3d3a3f1257563df6ad0cbec2403
David Krasnitsky <dikaveman@gmail.com>
2021-03-09 23:21:57 +0200

    Trying to scan the `slides` directory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use std::{error::Error, path::PathBuf, str::FromStr};
+use std::{error::Error, path::{Path, PathBuf}, str::FromStr};
 
-use office_align::high_zip::extract;
+// use office_align::high_zip::extract;
+
+use walkdir::{DirEntry, WalkDir};
 use zip::write::FileOptions;
-use zip_extensions::{zip_create_from_directory, zip_create_from_directory_with_options, zip_extract};
+use zip_extensions::{zip_create_from_directory_with_options, zip_extract};
+
+fn is_xml(entry: &DirEntry) -> bool {
+    entry.file_name()
+         .to_str()
+         .map(|s| s.ends_with(".xml"))
+         .unwrap_or(false)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Create a path:
@@ -16,8 +25,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let extraction_path =  PathBuf::from_str(r"D:\git\office-align\sample\test_out").unwrap();
     let new_archive = PathBuf::from_str(r"D:\git\office-align\sample\new_pptx.pptx").unwrap();
 
+    // let archive_file = Path::new(r"D:\git\office-align\sample\1.pptx");
+    // let extraction_path =  Path::new(r"D:\git\office-align\sample\test_out");
+    // let new_archive = Path::new(r"D:\git\office-align\sample\new_pptx.pptx");
+
     // Extract the `.pptx` file
     zip_extract(&archive_file,&extraction_path)?;
+
+    // TODO: Scan extracted directory slides
+    let slides_dir = PathBuf::from_str(r"D:\git\office-align\sample\new_pptx.pptx").unwrap();
+
+    let mut slides_path = extraction_path.clone();
+    slides_path.push("ppt");
+    slides_path.push("slides");
+
+    let walker = WalkDir::new(&slides_path).max_depth(1);
+    // for entry in walker.into_iter().filter_map(Result::ok).filter(|f|f.path().ends_with(".xml")) {
+    //     println!("{}", entry.path().display());
+    // }
+    walker.into_iter().filter_map(Result::ok).filter(|f|f.path().ends_with(".xml")).for_each(|f|{
+        println!("{}", f.path().display());
+    });
 
     // TODO: Detect all hebrew letters
     // TODO: Replace with reversed version
