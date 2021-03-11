a0d8d73f58f21d3c74c5a212b13bd75f6ee9b2a2
David Krasnitsky <dikaveman@gmail.com>
2021-03-11 17:49:57 +0200

    Implemented replacing Hebrew with Reversed Hebrew (pre-re-writing)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::{error::Error, fs::read_to_string, path::{Path, PathBuf}, str::FromStr};
+use std::collections::HashSet;
 use std::fs::File;
+use std::{
+    error::Error,
+    fs::read_to_string,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 // use office_align::high_zip::extract;
 
@@ -7,16 +13,37 @@ use walkdir::{DirEntry, WalkDir};
 use zip::write::FileOptions;
 use zip_extensions::{zip_create_from_directory_with_options, zip_extract};
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
 /// Returns `true` if a `DirEntry` is of given extension.
 fn file_extension(entry: &DirEntry, extension: &str) -> bool {
-    entry.file_name()
-         .to_str()
-         .map(|s| s.ends_with("." + extension))
-         .unwrap_or(false)
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.ends_with(extension))
+        .unwrap_or(false)
 }
 
 fn reverse(text: &str) -> String {
     text.chars().rev().collect()
+}
+
+fn extract_hebrew(s: &str) -> HashSet<&str> {
+    // `lazy_static!` to run once and prevent loops from needlessly repeating this section
+    lazy_static! {
+        // static ref RE: Regex = Regex::new(r"<a:t>[\u0590-\u05FF]+\s?[\u0590-\u05FF]*</a:t>").unwrap();
+        static ref RE: Regex = Regex::new(r#"[\u0590-\u05FF]+["']?[\u0590-\u05FF]*"#).unwrap();
+    }
+
+    let mut words_set = HashSet::new();
+    for found_match in RE.find_iter(s) {
+        words_set.insert(found_match.as_str());
+    }
+    // words_set.insert("test");
+    // iterate over all matches
+    // RE.find_iter(s).for_each(|m| words_set.insert(m.as_str()));  // WTF is the difference?
+    words_set
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -28,15 +55,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     // )?;
 
     let archive_file = PathBuf::from_str(r"D:\git\office-align\sample\1.pptx").unwrap();
-    let extraction_path =  PathBuf::from_str(r"D:\git\office-align\sample\test_out").unwrap();
+    let extraction_path = PathBuf::from_str(r"D:\git\office-align\sample\test_out").unwrap();
     let new_archive = PathBuf::from_str(r"D:\git\office-align\sample\new_pptx.pptx").unwrap();
 
     // let archive_file = Path::new(r"D:\git\office-align\sample\1.pptx");
     // let extraction_path =  Path::new(r"D:\git\office-align\sample\test_out");
     // let new_archive = Path::new(r"D:\git\office-align\sample\new_pptx.pptx");
 
+    println!("PreZip");
     // Extract the `.pptx` file
-    zip_extract(&archive_file,&extraction_path)?;
+    zip_extract(&archive_file, &extraction_path)?;
+    println!("PostZip");
 
     // TODO: Scan extracted directory slides
     let slides_dir = PathBuf::from_str(r"D:\git\office-align\sample\new_pptx.pptx").unwrap();
@@ -64,27 +93,42 @@ fn main() -> Result<(), Box<dyn Error>> {
     //     println!("{}", f.path().display());
     // });
 
-    fn str_strip_numbers(s: &str) -> Vec<i64> {
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r"\d+").unwrap();
-        }
-        // iterate over all matches
-        RE.find_iter(s)
-            // try to parse the string matches as i64 (inferred from fn type signature)
-            // and filter out the matches that can't be parsed (e.g. if there are too many digits to store in an i64).
-            .filter_map(|digits| digits.as_str().parse().ok())
-            // collect the results in to a Vec<i64> (inferred from fn type signature)
-            .collect()
-    }
+    // fn str_strip_numbers(s: &str) -> Vec<i64> {
+    //     lazy_static! {
+    //         static ref RE: Regex = Regex::new(r"\d+").unwrap();
+    //     }
+    //     // iterate over all matches
+    //     RE.find_iter(s)
+    //         // try to parse the string matches as i64 (inferred from fn type signature)
+    //         // and filter out the matches that can't be parsed (e.g. if there are too many digits to store in an i64).
+    //         .filter_map(|digits| digits.as_str().parse().ok())
+    //         // collect the results in to a Vec<i64> (inferred from fn type signature)
+    //         .collect()
+    // }
 
     //walker.into_iter().filter_map(Result::ok).filter(|file| file_extension(file, ".xml")).for_each(|file_path|{
-    for file_path in walker.into_iter().filter_map(Result::ok).filter(|file| file_extension(file, ".xml")) {
-        
+    for file_path in walker
+        .into_iter()
+        .filter_map(Result::ok) // Return only the `Result::ok` variants
+        .filter(|file| file_extension(file, ".xml"))
+    {
         println!("{}", file_path.path().display());
-        
+
         let data = read_to_string(file_path.path()).expect("Unable to load contents");
 
-        println!("data: {}", data);
+        //println!("data: {}", data);
+        let hebrew_words = extract_hebrew(&data);
+
+        let mut data = data.clone();
+        for word in hebrew_words {
+            //println!("File: {} Word: {}", file_path.path().display(), reverse(word));
+            data = data.replace(word, &reverse(word));
+        }
+
+        // println!("{}", data);
+        // for mat in Regex::new(r"\w").unwrap().find_iter(&data) {
+        //     println!("{:?}", mat);
+        // }
 
         // // Open and read the file entirely
         // let mut open_file = File::open(&file_path.path());
@@ -107,8 +151,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         //     }
 
-        // }   
-    };
+        // }
+    }
 
     // for entry in glob(slides_path.join("*.xml")).expect("Failed to read glob pattern") {
     //     match entry {
@@ -121,8 +165,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // TODO: Replace with reversed version
     // TODO: Attempt to find a way to properly align right
 
-    let options = FileOptions::default()
-        .compression_method(zip::CompressionMethod::Deflated);
+    let options = FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
 
     // Recreate the `.pptx` file
     zip_create_from_directory_with_options(&new_archive, &extraction_path, options)?;
